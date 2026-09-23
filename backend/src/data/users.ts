f784f42482a2a17f356798a703045cffd377c51d
kha9z <djarnor@gmail.com>
2026-09-23 20:24:02 +0200

    users and userrouter

---
+++ backend/src/data/users.ts
@@ -0,0 +1,12 @@
+import type { CombinedUserSchema } from '@project/shared'
+
+export const users: CombinedUserSchema[] = [
+  {
+    userId: 1,
+    name: 'Berit'
+  },
+  {
+    userId: 2,
+    name: 'Sandulf'
+  }
+]
--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -1,7 +1,60 @@
 import express, { type Router } from 'express'
+import { users } from '../data/users.ts'
+import { userIdParser } from '../middleware/idParsers.ts'
+import { userParser } from '../middleware/bodyParser.ts'
+import type { UserSchema } from '@project/shared'
 
 const router: Router = express.Router()
 
+router.get('/', (req, res) => {
+    res.status(200).json(users)
+})
 
+router.get('/:userId', userIdParser, (req, res) => {
+    const userId = res.locals.userId
+    const user = users.find(user => user.userId === userId)
+
+    if (!user) {
+        res.sendStatus(404)
+        return
+    }
+    res.status(200).json(user)
+})
+
+router.post('/', userParser, (req, res) => {
+    const body = req.body as UserSchema
+    const newUser = {
+        userId: users.length + 1,
+        name: body.name
+    }
+
+    users.push(newUser)
+    res.status(201).json(newUser)
+})
+
+router.put('/:userId', userIdParser, userParser, (req, res) => {
+    const userId = res.locals.userId
+    const body = req.body as UserSchema
+    const user = users.find(user => user.userId === userId)
+
+    if (!user) {
+        res.sendStatus(404)
+        return
+    }
+    user.name = body.name
+    res.status(200).json(user)
+})
+
+router.delete('/:userId', userIdParser, (req, res) => {
+    const userId = res.locals.userId
+    const index = users.findIndex(user => user.userId === userId)
+
+    if (index === -1) {
+        res.sendStatus(404)
+        return
+    }
+    users.splice(index, 1)
+    res.sendStatus(204)
+})
 
 export default router
